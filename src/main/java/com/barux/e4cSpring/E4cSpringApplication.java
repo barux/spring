f972307228540aa41da493e5fe237b14b33ec6c8
Davide <87716343+barux@users.noreply.github.com>
2023-12-20 09:51:41 +0100

    init proj

---
+++ src/main/java/com/barux/e4cSpring/E4cSpringApplication.java
@@ -0,0 +1,13 @@
+package com.barux.e4cSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E4cSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E4cSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barux/e4cSpring/E4cSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barux.e4cSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E4cSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
