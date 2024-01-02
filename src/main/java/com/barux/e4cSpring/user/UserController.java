de8e01ed5f66810885244d494d302130b15367fb
Davide <87716343+barux@users.noreply.github.com>
2024-01-02 15:39:48 +0100

    missing tag in config :)

--- src/main/java/com/barux/e4cSpring/user/UserController.java
+++ src/main/java/com/barux/e4cSpring/user/UserController.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class UserController {
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UserDTO[]> getAll() {
         return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
     }
